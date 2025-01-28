052aa95715dacf82dc4609e10ea908a2504b712d
afa <ahmetfakinci@gmail.com>
2025-01-28 21:29:12 +0300

    [fix] 2 app bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,7 @@ import 'katex/dist/katex.min.css';
 import 'prismjs/themes/prism-tomorrow.css';
 import 'react-notion-x/src/styles.css';
 
-import Header from '@/components/header/header';
 import Provider from '@/components/provider';
-import ScrollUpButton from '@/components/scroll-up-button';
 import '@/styles/globals.css';
 import '@/styles/paginate.css';
 
@@ -24,11 +22,7 @@ export default function RootLayout({
     <html lang="ko">
       <body className="text-primary bg-primary relative mx-auto mb-20 flex w-full max-w-screen-xl flex-col px-[10vw] md:px-[5vw]">
         <Provider>
-          <Header />
-          <main>{children}</main>
-          <div className="fixed bottom-12 right-10">
-            <ScrollUpButton />
-          </div>
+          {children}
         </Provider>
       </body>
     </html>
