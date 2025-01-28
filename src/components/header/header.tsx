4537259043c05af71922cccf3a8bce23232998c2
afa <ahmetfakinci@gmail.com>
2025-01-28 20:59:04 +0300

    [add] language selection

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -7,14 +7,13 @@ const NAV_ITEMS = [
   { path: 'blog', name: 'Blog' },
   { path: 'about', name: 'About' },
   { path: 'contact', name: 'Contact' },
-  { path: 'projects', name: 'Projects' },
 ];
 
 export default function Header() {
   return (
     <nav className="flex flex-col justify-between py-12 md:flex-row">
       <Link href="/" className="self-start md:self-auto">
-        <h1 className="text-3xl font-bold">Jihoo Kim</h1>
+        <h1 className="text-3xl font-bold">Ahmet Furkan Akıncı</h1>
       </Link>
       <div className="my-6 flex space-x-8 self-center md:my-0 md:self-auto">
         <ul className="flex space-x-8">
